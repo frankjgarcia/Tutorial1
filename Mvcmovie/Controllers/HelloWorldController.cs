79e97275b1d19ed5c35bd0f8e4469e037b6b9fab
Frank Garcia <frankjgarcia@outlook.com>
2019-02-06 22:43:05 -0600

    Enter

---
+++ Mvcmovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Mvcmovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int NumTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = NumTimes;
+
+            return View();
+        }
+
+    }
+}
